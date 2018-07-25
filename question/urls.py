cefbc9888c9c341f007b58a5e311c26937c25778
rinhye <pauline.lam24@gmail.com>
2018-07-25 17:20:52 +0200

    Route propre pour récupération

--- question/urls.py
+++ question/urls.py
@@ -6,5 +6,5 @@ from django.urls import path, include
 #Routes de l'application
 urlpatterns = [
 	path('', views.index, name='index'),
-	path('/question_du_jour', views.question_du_jour, name='question_du_jour')
+	path('question_du_jour', views.question_du_jour, name='question_du_jour')
 ]
