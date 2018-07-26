3bbbbca1575551bdbe36d150c389bfc026602324
rinhye <pauline.lam24@gmail.com>
2018-07-26 06:32:14 +0200

    merge

--- question/urls.py
+++ question/urls.py
@@ -7,8 +7,8 @@ from django.conf.urls import url
 #Routes de l'application
 urlpatterns = [
 	url(r'^$', login, {'template_name': 'accounts/login.html'}, name='index'),
-    path(r'^logout/$', logout, {'template_name': 'accounts/logout.html'}, name='logout' ),
-	path('', views.index, name='index'),
+    path('logout', logout, {'template_name': 'accounts/logout.html'}, name='logout' ),
+	path('inscription', views.inscription, name='inscription'),
 	path('matches', views.matches, name='matches'),
 	path('question_du_jour', views.question_du_jour, name='question_du_jour')
 ]
